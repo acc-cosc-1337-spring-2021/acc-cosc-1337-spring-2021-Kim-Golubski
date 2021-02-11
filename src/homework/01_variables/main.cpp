3cb8952c459a7d0f3f99ab0924020b4836f0c072
Github Username <kimberly.golubski@g.austincc.edu>
2021-02-11 14:30:07 -0600

    Homework 2 Receipt

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,15 +1,37 @@
 ﻿//write include statements
-
+#include "variables.h"
+#include <iostream>
 
 //write namespace using statement for cout
-
+using std::cin; using std::cout;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	double meal_amount;
+	double tip_rate;
+	double tip_amount;
+	double tax_amount;
+	double total;
+
+	cout<<"What was the meal amount? ";
+	cin>>meal_amount;
+
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout<<"What was your tip rate? ";
+	cin>>tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	total = tip_amount + tax_amount + meal_amount;
+
+	cout<<"Meal Amount: "<<meal_amount<<"\n";
+	cout<<"Sales Tax: "<<tax_amount<<"\n";
+	cout<<"Tip Amount:"<<tip_amount<<"\n";
+	cout<<"Toatl: "<<total<<"\n";
 
 	return 0;
 }
